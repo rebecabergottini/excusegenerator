319c94006cdda21304a80e6acec6db06fe174c83
rebecabergottini <rebeca._bergo@hotmail.com>
2022-12-30 20:47:55 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+/* eslint-disable*/
+// import "~bootstrap/dist/css/bootstrap.min.css";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let array4 = Math.floor(Math.random() * who.length);
+  let array5 = Math.floor(Math.random() * what.length);
+  let array6 = Math.floor(Math.random() * when.length);
+  document.getElementById("excuse").innerHTML =
+    who[array4] + " " + what[array5] + " " + when[array6];
 };
